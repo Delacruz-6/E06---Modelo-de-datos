1e1c0a18b71ac5e189f3c50b61abd8a4cdef982c
Delacruz-6 <gdlcruzguzman@gmail.com>
2021-10-24 20:40:12 +0200

    fixed errors

--- E06-EJ2-ModelosDeDatos/src/main/java/com/salesianostriana/dam/E06EJ2ModelosDeDatos/MainDePrueba.java
+++ E06-EJ2-ModelosDeDatos/src/main/java/com/salesianostriana/dam/E06EJ2ModelosDeDatos/MainDePrueba.java
@@ -29,50 +29,55 @@ public class MainDePrueba {
                 .build();
         profesorService.save(profesor1);
 
-        CursoOnline dam2 = CursoOnline.builder()
-                .nombre("2º DAM")
+        CursoOnline java = CursoOnline.builder()
+                .nombre("Java desde 0")
+                .puntuación(8.8)
+                .profesor(profesor1)
+                .build();
+        java.addProfesor(profesor1);
+        service.save(java);
+
+        CursoOnline jpa = CursoOnline.builder()
+                .nombre("Curso avanzado JPA")
                 .puntuación(7.8)
                 .profesor(profesor1)
                 .build();
-        //profesor1.getCursos().add(dam2);
-        dam2.addProfesor(profesor1);
-        service.save(dam2);
+        jpa.addProfesor(profesor1);
+        service.save(jpa);
 
-        Video dam2V1 = Video.builder()
+        Video jpaV1 = Video.builder()
                 .titulo("JPA ciclo de vida")
                 .orden("3º parte")
                 .descripcion("En esta parte del videotutorial hablaremos de los ciclos de vida de JPA")
                 .url("www.salesianosTriana.com")
-                .curso(dam2)
+                .curso(java)
                 .build();
-        dam2.addVideo(dam2V1);
+        jpa.addVideo(jpaV1);
 
-        Video dam2V2 = Video.builder()
+        Video jpaV2 = Video.builder()
                 .titulo("Consultas con JPA")
                 .orden("4º parte")
                 .descripcion("En esta parte del videotutorial hablaremos de consultas que podemos utlizar con JPA")
                 .url("www.salesianosTriana.com")
-                .curso(dam2)
+                .curso(jpa)
                 .build();
-        dam2.addVideo(dam2V2);
+        jpa.addVideo(jpaV2);
 
 
-        System.out.printf("Nombre del curso, del profesor:");
-        System.out.printf("%s, %s\n", dam2.getNombre(), dam2.getProfesor().getNombre());
-        System.out.printf("Nombre del profesor y numero de cursos y cantidad de videos:");
-        System.out.printf("%s, %s, %s\n", profesor1.getNombre(), profesor1.getCursos().size(), dam2.getVideoLists().size());
-        System.out.println("Video:");
-        System.out.printf("%s, %s\n", dam2V1.getTitulo(), dam2V1.getDescripcion(),dam2V1.getCurso());
+        System.out.printf("Nombre del curso y correo del profesor que lo imparte: ");
+        System.out.printf("%s, %s\n", java.getNombre(), java.getProfesor().getEmail());
+        System.out.printf("Nombre del profesor y numero de cursos y nombre - cantidad de videos curso jpa: ");
+        System.out.printf("%s, %s, %s\n", profesor1.getNombre(), profesor1.getCursos().size(), profesor1.getCursos().get(1).getNombre() +" - "+ profesor1.getCursos().get(1).getVideoLists().size());
+        System.out.printf("JPA Nombre del curso, cantidad de videos, titulo 1º video y descripcion 1º video:");
+        System.out.printf("%s, %s, %s, %s\n", jpa.getVideoLists().get(1).getCurso().getNombre(), jpa.getVideoLists().size(), jpa.getVideoLists().get(1).getTitulo(), jpa.getVideoLists().get(1).getDescripcion());
 
-
-
-
-
-
-
-
-
-
+        //Comprobacion borrado
+        profesor1.removeCurso(jpa);
+        java.removeProfesor(profesor1);
+        System.out.printf("Nombre del profesor, numero de cursos, datos curso JPA de ese profesor: ");
+        System.out.printf("%s, %s, %s \n", profesor1.getNombre(), profesor1.getCursos().size(), profesor1.getCursos());
+        System.out.printf("Profesor del curso JAVA: ");
+        System.out.printf("%s \n",java.getProfesor());
     }
 
 }
